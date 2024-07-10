7fc6dc25c50f1d16fd7ae3f7ad736d163f454601
H-Stein <henry.stein@pnnl.gov>
2024-07-10 10:48:11 -0400

    Added a world map
    The world map outline allows us to get a better idea of where the data came from.

--- DroughtAnalysis.R
+++ DroughtAnalysis.R
@@ -7,5 +7,6 @@ srdbData %>%
 
 # Sanity check: plot the data
 library(ggplot2)
-p <- ggplot(srdb_filtered, aes(Longitude, Latitude, color = Rs_annual)) + geom_point()
+world_coords <- map_data("world")
+p <- ggplot(srdb_filtered, aes(Longitude, Latitude, color = Rs_annual)) + geom_map(data = world_coords, map = world_coords, aes(long, lat, map_id = region), color = "white", fill = "white", linewidth = 0.2) + geom_point()
 print(p)
