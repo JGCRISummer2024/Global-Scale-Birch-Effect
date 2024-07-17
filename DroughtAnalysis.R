d173b02fe1075df9864461189107d59c8f1e2d83
H-Stein <henry.stein@pnnl.gov>
2024-07-17 16:18:52 -0400

    First SPEI analysis
    SPEI_analysis imports SPEI data on a 12 month scale, meaning that it considers rainfall from the entire preceding year when calculating the SPEI. Right now, it makes a chart of the historical SPEI values for Boise City, OK, clearly demonstrating the dust bowl drought in the 1930s.

--- DroughtAnalysis.R
+++ DroughtAnalysis.R
@@ -2,30 +2,32 @@ library(ggplot2)
 library(dplyr)
 library(gridExtra)
 
-
 srdbData <- read.csv("srdb-data.csv") # get data
+
 srdbData %>% 
-  select(Latitude, Longitude, Study_midyear, Rs_annual, Rs_growingseason, Study_midyear) %>% 
+  select(Latitude, Longitude, Study_midyear, Rs_annual, Rs_growingseason, Study_midyear, Ecosystem_type) %>% 
   filter(!is.na(Rs_annual) | !is.na(Rs_growingseason)) ->
   srdb_filtered # srdb data that has data in either rs_annual, rs_growingseason, or both
 rsRange <- qplot(srdb_filtered$Rs_annual, geom = "boxplot", xlab = "Annual C flux values g / cm^2")
 
-srdb_filtered %>% 
-  filter(Rs_annual >= 0 & Rs_annual <= (1100 + 1.5 * (1100 - 486.5))) ->
-  srdb_filtered_no_outliers
 
+srdb_filtered %>%
+  filter(Rs_annual >= 0 & Rs_annual <= 4500) ->
+  srdb_filtered_no_outliers
+print(max(srdb_filtered_no_outliers$Rs_annual))
 # world map with data, colored by annual C flux
 world_coords <- map_data("world")
 p <- ggplot(srdb_filtered_no_outliers, aes(Longitude, Latitude, color = Rs_annual)) + geom_map(data = world_coords, map = world_coords, aes(long, lat, map_id = region), color = "white", fill = "white", linewidth = 0.2) + geom_point()
 
-# boxplot of the years the data was collected
-yearPlot <- qplot(srdb_filtered$Study_midyear, geom = "boxplot", xlab = "Year of data collection")
-
-# annual C flux boxplot without the extreme values from before
-rsRangeNoOutliers <- qplot(srdb_filtered_no_outliers$Rs_annual, geom = "boxplot", xlab = "Annual C flux values g / cm^2 without extreme values")
-
-# plot all the graphs
-grid.arrange(p, yearPlot, rsRange, rsRangeNoOutliers, nrow = 2)
-
-
-
+print(p)
+# # boxplot of the years the data was collected
+# yearPlot <- qplot(srdb_filtered$Study_midyear, geom = "boxplot", xlab = "Year of data collection")
+# 
+# # annual C flux boxplot without the extreme values from before
+# rsRangeNoOutliers <- qplot(srdb_filtered_no_outliers$Rs_annual, geom = "boxplot", xlab = "Annual C flux values g / cm^2 without extreme values")
+# 
+# # plot all the graphs
+# grid.arrange(p, yearPlot, rsRange, rsRangeNoOutliers, nrow = 2)
+# 
+# 
+# 
---
+++ SPEI_analysis.R
@@ -0,0 +1,46 @@
+# SPEI
+library(ggplot2)
+library(dplyr)
+library(tibble)
+coords <- tribble(
+  ~place,  ~lon,         ~lat,
+  "Boise City", -102.51464, 36.73183
+)
+coords$ID <- seq_len(nrow(coords))
+
+
+# Documentation: https://spei.csic.es/database.html
+# Data downloaded 2024-07-17
+
+# These data are provided as netCDF files (http://en.wikipedia.org/wiki/NetCDF)
+# Happily, the terra package can handle these!
+# (There's also the `ncdf4` package for many other applications)
+library(terra)
+spei <- rast("spei12.nc")
+# Note that we've told Git to IGNORE these data files; see ".gitignore" file
+
+# Confirm that things look good -- it's a spatial raster object,
+# global half degree resolution, from January 1901 to December 2022
+print(spei)
+
+# Extract our points of interest. terra::extract() will handle making sure
+# the coordinates get mapped to the correct grid cell(s) in the data
+spei_coords <- terra::extract(spei, coords[2:3])
+# 1465 columns! Why? Because that's 122 years * 12 months + "ID" column
+
+# Reshape data into a more manageable form
+library(tidyr)
+spei_monthly <- pivot_longer(spei_coords, -ID)
+spei_monthly <- separate(spei_monthly, name, into = c("spei", "entry"), convert = TRUE)
+# The SPEI data don't seem to provide 'time' explicitly in the netcdf, so
+# compute it from the entries
+spei_monthly$year <- ceiling(spei_monthly$entry / 12) + 1900
+spei_monthly$month <- (spei_monthly$entry - 1) %% 12 + 1
+spei_monthly$time <- with(spei_monthly, year + (month-1) / 12)
+spei_monthly <- dplyr::left_join(spei_monthly, coords, by = "ID")
+
+# Not sure about the best way to plot this
+p <- ggplot(spei_monthly, aes(time, value, color = place)) +
+  geom_point(size = 0.25) + geom_area() + 
+  scale_x_discrete(name = "Year", limits = seq(from = 1900, to = 2020, by = 10))
+print(p)
